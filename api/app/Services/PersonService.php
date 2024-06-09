dd0b66a6fbb3869a532cacbfb6f8285c53bdb9da
Douglas Gondim <douglasgondimp@gmail.com>
2024-06-09 12:44:51 -0300

    (feat:api) Cria classe de serviço `App\Services\PersonService`
    + Adiciona os métodos na classe:
    `index`
    `create`
    `show`
    `update`
    `destroy`

---
+++ api/app/Services/PersonService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Person;
+use PhpParser\Node\Expr\FuncCall;
+
+class PersonService
+{
+    protected $model = Person::class;
+
+    public function index()
+    {
+        return $this->model::all();
+    }
+
+    public function create(array $request)
+    {
+        return $this->model::create($request);
+    }
+
+    public function show(Person $person)
+    {
+        return $person;
+    }
+
+    public function update(array $request, Person $person)
+    {
+        $person->update($request);
+
+        return $person;
+    }
+
+    public function destroy(Person $person)
+    {
+        $person->delete();
+
+        return $person;
+    }
+}
