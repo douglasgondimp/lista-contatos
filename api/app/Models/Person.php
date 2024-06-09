087b7d1186670f4acb1dfb9a15a06e9c5e5a8be9
Douglas Gondim <douglasgondimp@gmail.com>
2024-06-09 12:35:08 -0300

    (feat:api) Cria classe modelo `App\Models\Person`

---
+++ api/app/Models/Person.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Person extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'people';
+    protected $fillable = ['name', 'email', 'cpf', 'birth_date', 'phone'];
+    protected $casts = [
+        'name'       => 'string',
+        'email'      => 'string',
+        'cpf'        => 'string',
+        'birth_date' => 'date',
+        'phone'      => 'string',
+    ];
+}
