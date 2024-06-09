62cd863620a0178f832ad02fe73e784aecfa2a52
Douglas Gondim <douglasgondimp@gmail.com>
2024-06-09 13:24:49 -0300

    (feat:api) Cria a tabela `contacts`

---
+++ api/database/migrations/2024_06_09_161810_create_contacts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('person_id')->constrained('people');
+            $table->string('name', 150);
+            $table->string('email', 150)->nullable();
+            $table->string('phone', 11)->nullable();
+            $table->string('whatsapp', 11)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
