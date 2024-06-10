fbc812da82d57c53e107b86883d517ac31e3efaf
Douglas Gondim <douglasgondimp@gmail.com>
2024-06-10 20:42:27 -0300

    (feat:api) Adiciona rota para listar os contatos das pessoas

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,5 +4,6 @@ use App\Http\Controllers\ContactController;
 use App\Http\Controllers\PersonController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('people/{person}/contacts', [PersonController::class, 'getContacts']);
 Route::apiResource('people', PersonController::class);
 Route::apiResource('contact', ContactController::class)->except('index');
