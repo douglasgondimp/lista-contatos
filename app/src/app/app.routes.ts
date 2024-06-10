8c4330f3b85af87571368b69a86f95756b66ddec
Douglas Gondim <douglasgondimp@gmail.com>
2024-06-10 19:59:09 -0300

    (feat:app) Cria rota para a edição das pessoas e lista de contatos

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { PeopleComponent } from './view/people/people.component';
+import { ContactsListComponent } from './components/contacts/contacts-list/contacts-list.component';
+import { PeopleEditComponent } from './components/people/people-edit/people-edit.component';
 
 export const routes: Routes = [
     {
@@ -9,6 +11,16 @@ export const routes: Routes = [
     },
     {
         path: 'pessoas',
-        component: PeopleComponent
+        component: PeopleComponent,
+        children: [
+            {
+                path: ':id',
+                component: PeopleEditComponent
+            },
+            {
+                path: ':id/contatos',
+                component: ContactsListComponent
+            }
+        ]
     }
 ];
