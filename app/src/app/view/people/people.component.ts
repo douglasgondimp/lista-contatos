a09aff91c20e06f1b0520682bef3e6b0556d24b5
Douglas Gondim <douglasgondimp@gmail.com>
2024-06-10 19:58:06 -0300

    (feat:app) Adiciona tabela na view das pessoas
    `src/app/view/people`
    + Cria variavel que armazena os dados retornados da API com a lista de pessoas
    + Mostra o resultado da lista em formato de tabela

--- app/src/app/view/people/people.component.ts
+++ app/src/app/view/people/people.component.ts
@@ -1,12 +1,26 @@
+import { NgFor } from '@angular/common';
+import { PeopleService } from '../../services/people.service';
+import { Person } from './../../models/person.model';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-people',
   standalone: true,
-  imports: [],
+  imports: [NgFor, RouterLink],
+  providers: [PeopleService],
   templateUrl: './people.component.html',
   styleUrl: './people.component.scss'
 })
 export class PeopleComponent {
+  people: Person[] = [];
+
+  constructor(private peopleService: PeopleService) { }
+
+  ngOnInit(): void {
+    this.peopleService.getAll().subscribe((people) => {
+      this.people = people;
+    });
+  }
 
 }
