1823fc7736c47581adea3293cecac7a472431b48
Douglas Gondim <douglasgondimp@gmail.com>
2024-06-10 20:40:04 -0300

    (feat:app) Altera retorno da lista de pessoas na service

--- app/src/app/services/people.service.ts
+++ app/src/app/services/people.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Person } from "../models/person.model";
 import { Observable } from "rxjs";
+import { Pagination } from "../models/pagination.model";
 
 @Injectable({
     providedIn: 'root',
@@ -13,7 +14,7 @@ export class PeopleService {
         private http: HttpClient
     ){}
 
-    getAll(): Observable<Person[]>{
-        return this.http.get<Person[]>(`${this.baseUrl}`);
+    getAll(): Observable<Pagination>{
+        return this.http.get<Pagination>(`${this.baseUrl}`);
     }
 }
