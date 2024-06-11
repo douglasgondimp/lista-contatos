09d7b2200834da410a45c3a8491490a8e220322f
Douglas Gondim <douglasgondimp@gmail.com>
2024-06-10 21:58:49 -0300

    (feat:app) Adiciona métodos na service
    `src/app/services/peope.service.ts`
    + Adiciona o método `getByID` para buscar os dados da pessoa na API
    + Adiciona o método `getContacts` para buscar os dados dos contatos da pessoa

--- app/src/app/services/people.service.ts
+++ app/src/app/services/people.service.ts
@@ -17,4 +17,12 @@ export class PeopleService {
     getAll(): Observable<Pagination>{
         return this.http.get<Pagination>(`${this.baseUrl}`);
     }
+
+    getByID(id: number): Observable<Person>{
+        return this.http.get<Person>(`${this.baseUrl}/${id}`);
+    }
+
+    getContacts(id: number): Observable<Pagination> {
+        return this.http.get<Pagination>(`${this.baseUrl}/${id}/contacts`);
+    }
 }
