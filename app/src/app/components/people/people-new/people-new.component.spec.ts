3d7b7d4681abd2a05a917bbfaeb8d81f4351c0c3
Douglas Gondim <douglasgondimp@gmail.com>
2024-06-11 20:03:10 -0300

    (feat:app) Cria o componente `people-new`

---
+++ app/src/app/components/people/people-new/people-new.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PeopleNewComponent } from './people-new.component';
+
+describe('PeopleNewComponent', () => {
+  let component: PeopleNewComponent;
+  let fixture: ComponentFixture<PeopleNewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PeopleNewComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(PeopleNewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ app/src/app/components/people/people-new/people-new.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-people-new',
+  standalone: true,
+  imports: [],
+  templateUrl: './people-new.component.html',
+  styleUrl: './people-new.component.scss'
+})
+export class PeopleNewComponent {
+
+}
