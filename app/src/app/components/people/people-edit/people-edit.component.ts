e710a256d611eea09a165546c7ab546cc7ab93dc
Douglas Gondim <douglasgondimp@gmail.com>
2024-06-11 21:56:11 -0300

    (feat:app) Cria layout da página de edição de pessoas

--- app/src/app/components/people/people-edit/people-edit.component.ts
+++ app/src/app/components/people/people-edit/people-edit.component.ts
@@ -1,12 +1,31 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component } from '@angular/core';
+import { FormPersonComponent } from '../../forms/form-person/form-person.component';
+import { Person } from '../../../models/person.model';
+import { PeopleService } from '../../../services/people.service';
 
 @Component({
   selector: 'app-people-edit',
   standalone: true,
-  imports: [],
+  imports: [FormPersonComponent],
+  providers: [PeopleService],
   templateUrl: './people-edit.component.html',
   styleUrl: './people-edit.component.scss'
 })
 export class PeopleEditComponent {
+  personId!: number;
+  person?: Person;
 
+  constructor(
+    private actRoute: ActivatedRoute,
+    private peopleService: PeopleService
+  ) { }
+
+  ngOnInit(): void {
+    this.personId = this.actRoute.snapshot.params['id'];
+
+    this.peopleService.getByID(this.personId).subscribe((person) => {
+      this.person = person;
+    });
+  }
 }
