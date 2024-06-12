ec03a6b23069a492f21bf64ebb076bd17a2291c7
Douglas Gondim <douglasgondimp@gmail.com>
2024-06-11 21:55:45 -0300

    (feat:app) Cria layout para página de formulário de cadastro de pessoa

--- app/src/app/components/people/people-new/people-new.component.ts
+++ app/src/app/components/people/people-new/people-new.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormPersonComponent } from '../../forms/form-person/form-person.component';
 
 @Component({
   selector: 'app-people-new',
   standalone: true,
-  imports: [],
+  imports: [FormPersonComponent],
   templateUrl: './people-new.component.html',
   styleUrl: './people-new.component.scss'
 })
