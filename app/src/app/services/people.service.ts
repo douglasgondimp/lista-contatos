5a3085b45085250491fefb6db2ce9977b61a4bc3
Douglas Gondim <douglasgondimp@gmail.com>
2024-06-13 21:17:15 -0300

    (feat:app) Cria métodos para enviar dados de criação e edição para a API
    Métodos criados para as pessoas

--- app/src/app/services/people.service.ts
+++ app/src/app/services/people.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 import { Person } from "../models/person.model";
 import { Observable } from "rxjs";
 import { Pagination } from "../models/pagination.model";
+import { Contact } from "../models/contact.mode";
 
 @Injectable({
     providedIn: 'root',
@@ -14,15 +15,23 @@ export class PeopleService {
         private http: HttpClient
     ){}
 
-    getAll(): Observable<Pagination>{
-        return this.http.get<Pagination>(`${this.baseUrl}`);
+    getAll(): Observable<Person[]>{
+        return this.http.get<Person[]>(`${this.baseUrl}`);
     }
 
     getByID(id: number): Observable<Person>{
         return this.http.get<Person>(`${this.baseUrl}/${id}`);
     }
 
-    getContacts(id: number): Observable<Pagination> {
-        return this.http.get<Pagination>(`${this.baseUrl}/${id}/contacts`);
+    getContacts(id: number): Observable<Contact[]> {
+        return this.http.get<Contact[]>(`${this.baseUrl}/${id}/contacts`);
+    }
+
+    update(person: Person): Observable<Person>{
+        return this.http.put<Person>(`${this.baseUrl}/${person.id}`, person);
+    }
+
+    create(person: Person): Observable<Person>{
+        return this.http.post<Person>(`${this.baseUrl}`, person);
     }
 }
