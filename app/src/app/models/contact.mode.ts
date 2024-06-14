8a9f0c3eaf9e38319d108168536833a45097ed90
Douglas Gondim <douglasgondimp@gmail.com>
2024-06-14 19:33:43 -0300

    (fix:app) Ajusta campos na model Contact

--- app/src/app/models/contact.mode.ts
+++ app/src/app/models/contact.mode.ts
@@ -1,11 +1,11 @@
 export interface Contact {
-    id?: number;
-    person_id: number;
-    name: string;
-    email: string|null;
-    phone: string|null;
-    whatsapp: string|null;
-    created_at?: Date|null;
-    updated_at?: Date|null;
-    deleted_at?: Date|null;
+    id: number|null;
+    person_id?: number;
+    name?: string;
+    email: string;
+    phone: string;
+    whatsapp: string;
+    created_at?: Date;
+    updated_at?: Date;
+    deleted_at?: Date;
 }
