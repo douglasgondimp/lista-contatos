c1f53489e396a65eab903f67e9a3d7c004596cbb
Douglas Gondim <douglasgondimp@gmail.com>
2024-06-13 21:59:50 -0300

    (feat:app) Cria métodos na service `ContactService`
    + Cria método `getByID`
    + Cria método `update`
    + Cria método `create`

--- app/src/app/services/contact.service.ts
+++ app/src/app/services/contact.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Contact } from "../models/contact.mode";
+import { Observable } from "rxjs";
 
 @Injectable({
     providedIn: 'root',
@@ -10,4 +12,16 @@ export class ContactService {
     constructor(
         private http: HttpClient
     ){}
+
+    getByID(id: number): Observable<Contact>{
+        return this.http.get<Contact>(`${this.baseUrl}/${id}`);
+    }
+
+    update(contact: Contact): Observable<Contact>{
+        return this.http.put<Contact>(`${this.baseUrl}/${contact.id}`, contact);
+    }
+
+    create(contact: Contact): Observable<Contact>{
+        return this.http.post<Contact>(`${this.baseUrl}`, contact);
+    }
 }
