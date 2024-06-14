ef35a2fcf38b9ab8991729b2e3155eea7161818e
Douglas Gondim <douglasgondimp@gmail.com>
2024-06-14 19:33:15 -0300

    (fix:api) Altera rota resource para os contatos
    `routes/api`
    - troca rota `contact` para `contacts`

--- api/routes/api.php
+++ api/routes/api.php
@@ -6,4 +6,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('people/{person}/contacts', [PersonController::class, 'getContacts']);
 Route::apiResource('people', PersonController::class);
-Route::apiResource('contact', ContactController::class)->except('index');
+Route::apiResource('contacts', ContactController::class)->except('index');
