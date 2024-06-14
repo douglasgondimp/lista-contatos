32c0286ce73f33c1a66b8b138a5d5202c4e62ade
Douglas Gondim <douglasgondimp@gmail.com>
2024-06-13 21:16:05 -0300

    (feat:app) Adiciona mascaras de campos e envia o formulário de pessoas

--- app/src/app/components/forms/form-person/form-person.component.ts
+++ app/src/app/components/forms/form-person/form-person.component.ts
@@ -1,24 +1,34 @@
 import { Component, Input } from '@angular/core';
 import { Person } from '../../../models/person.model';
 import { FormsModule } from '@angular/forms';
+import { PeopleService } from '../../../services/people.service';
+import { Router } from '@angular/router';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 
 @Component({
   selector: 'app-form-person',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgxMaskDirective],
+  providers: [PeopleService, provideNgxMask()],
   templateUrl: './form-person.component.html',
   styleUrl: './form-person.component.scss'
 })
 export class FormPersonComponent {
   @Input() dataPerson?: Person
+  protected router!: Router;
 
   person: Person = {
+    id: null,
     name: '',
     cpf: '',
     email: '',
     phone: '',
     birth_date: null
   }
+
+  constructor(
+    protected peopleService: PeopleService
+  ) { }
 
   ngOnInit(): void {
     if (this.dataPerson) {
@@ -27,6 +37,24 @@ export class FormPersonComponent {
   }
 
   sendForm() {
-    console.log("dados enviados");
+    if (this.person.id) {
+      this.peopleService.update(this.person).subscribe(
+        (response) => {
+          this.router.navigate(['/people']);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    } else {
+      this.peopleService.create(this.person).subscribe(
+        (response) => {
+          this.router.navigate(['/people']);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
   }
 }
