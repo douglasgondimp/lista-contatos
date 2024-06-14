2eeb4f5fbfd8c5a20f341fbf12920e905f8e91dc
Douglas Gondim <douglasgondimp@gmail.com>
2024-06-13 22:00:21 -0300

    (feat:app) Cria rota para adicionar novo contato na pessoa

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { PeopleComponent } from './view/people/people.component';
 import { ContactsListComponent } from './components/contacts/contacts-list/contacts-list.component';
 import { PeopleEditComponent } from './components/people/people-edit/people-edit.component';
 import { PeopleNewComponent } from './components/people/people-new/people-new.component';
+import { ContactsNewComponent } from './components/contacts/contacts-new/contacts-new.component';
 
 export const routes: Routes = [
     {
@@ -25,5 +26,9 @@ export const routes: Routes = [
     {
         path: 'pessoas/:id/contatos',
         component: ContactsListComponent
+    },
+    {
+        path: 'pessoas/:id/novo-contato',
+        component: ContactsNewComponent
     }
 ];
